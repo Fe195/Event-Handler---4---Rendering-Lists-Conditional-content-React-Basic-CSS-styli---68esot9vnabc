f3d9f8e6daf6e7cb541b241e0226a92450a83a7c
Fe195 <114395573+Fe195@users.noreply.github.com>
2022-12-03 00:46:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,19 @@ import '../styles/App.css';
 const App = () => {
 
   const handleDoubleClick = (event) =>{
-  
+  console.log('I was double clicked');
+  }
+   const handleClick = (event) =>{
+  console.log('I was not double clicked');
   }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn" onClick={handleClick} onDoubleClick={handleDoubleClick}>Double click me</button>
     </div>
   )
 }
 
 
 export default App;
+Footer
+© 2022 GitHub, Inc.
